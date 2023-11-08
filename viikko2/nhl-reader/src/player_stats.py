c6bb88a9a5b489c5bf18e3137d9cefca59c78a97
Juho <JuhoPaananen@users.noreply.github.com>
2023-11-08 14:28:13 +0200

    W2 Ex12 remove unused import

--- viikko2/nhl-reader/src/player_stats.py
+++ viikko2/nhl-reader/src/player_stats.py
@@ -1,14 +1,12 @@
-from player_reader import PlayerReader
-
 class PlayerStats:
     def __init__(self, reader):
         self.reader = reader
 
-    def _filter_by_nationality(self, player, nationality):
+    def check_nationality(self, player, nationality):
         return player.nationality == nationality
 
     def top_scorers_by_nationality(self, nationality):
         players = self.reader.get_players()
-        filtered_players = filter(lambda player: self._filter_by_nationality(player, nationality), players)
+        filtered_players = filter(lambda player: self.check_nationality(player, nationality), players)
         sorted_players = sorted(filtered_players, key=lambda player: player.points(), reverse=True)
         return sorted_players
