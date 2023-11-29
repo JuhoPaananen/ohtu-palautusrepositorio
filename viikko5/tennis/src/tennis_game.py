1f32247e4204188da17f61e5f1cd4a4082f6e7d3
Juho <JuhoPaananen@users.noreply.github.com>
2023-11-29 08:57:04 +0200

    Refraktorointi

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,9 +1,6 @@
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.players = [player1_name, player2_name]
 
     def won_point(self, player_name):
         if player_name == "player1":
